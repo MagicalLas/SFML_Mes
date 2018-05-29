cea70aa900ff8b9785338f59823635b108a8b200
Wonho ha <35015234+Las-Wonho@users.noreply.github.com>
2018-05-29 19:22:46 +0900

    Project Setting Complete

--- Visual Project/Visual Project/Maincpp.cpp
+++ Visual Project/Visual Project/Maincpp.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 
-int main() {
-	std::cout << "Las Start";
+
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+	std::cout << "Program Start - Las\n";
+	sf::RenderWindow window(sf::VideoMode(800, 800), "PLAY - LAS");
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		window.display();
+	}
+
+	std::cout << "Program End - Las\n";
+	return 0;
 }
