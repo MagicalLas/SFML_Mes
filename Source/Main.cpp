fcfb45243e527bf002ad3f2f825713c6d076fa89
Wonho ha <35015234+Las-Wonho@users.noreply.github.com>
2018-05-30 10:50:28 +0900

    되돌리기.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,18 +1,13 @@
-//STD
+﻿//STD
 #include<iostream>
 //Other
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
+#include<SFML/Graphics.hpp>
 //Wonho
 #include"../Include/Resource.h"
 #include"../Include/Useful.h"
 
-int main(){
-	
-	
-	std::cout <<"Biuld Success" << std::endl;
+int main() {
+	std::cout << "Biuld Success" << std::endl;
 	std::cout << "Program Start!" << std::endl;
 
 	//WON API
@@ -20,29 +15,42 @@ int main(){
 	Wonho_pp::String_Helper StringHelper;
 	Wonho_pp::Screen_Move ScreenHelper;
 
-	sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 800), "PLAY - LAS");
 
-	sf::Clock deltaClock;
+	sf::Text text;
+	sf::Font font;
+
+	if (!font.loadFromFile(res.getPath("font.ttf")))
+	{
+		std::cout << "Font can't find\n";
+	}
+	text.setFont(font);
+
+	text.setString(L"³ª´Â ³ª");
+	text.setCharacterSize(24);
+	ScreenHelper.setMiddle(text);
+	text.setPosition(400, 400);
+	text.setFillColor(sf::Color::White);
+	text.setStyle(sf::Text::Bold);
+
+
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			
 			if (event.type == sf::Event::Closed)
-			{
 				window.close();
+			if (event.type == sf::Event::MouseButtonPressed) {
 			}
 		}
 
 		window.clear();
-		window.draw(shape);
+
+		window.draw(text);
 		window.display();
 	}
 
-	
 	std::cout << "Program End - Las\n";
 	return 0;
 }
