e27f36372e71c422d328921e604d6aa8a3f0afb6
Wonho ha <35015234+Las-Wonho@users.noreply.github.com>
2018-05-30 10:46:48 +0900

    delete imgui

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -22,45 +22,30 @@ int main(){
 	Wonho_pp::String_Helper StringHelper;
 	Wonho_pp::Screen_Move ScreenHelper;
 
-
-	sf::RenderWindow window(sf::VideoMode(800, 800), "PLAY - LAS");
+	sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
 	window.setFramerateLimit(60);
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-	ImGui::SFML::Init(window);
-	sf::Text text;
-	sf::Font font;
-	
-	if (!font.loadFromFile(res.getPath("font.ttf")))
-	{
-		std::cout << "Font can't find\n";
-	}
-	text.setFont(font);
-
-	text.setString(L"Test Text");
-	text.setCharacterSize(24);
-	ScreenHelper.setMiddle(text);
-	text.setPosition(400, 400);
-	text.setFillColor(sf::Color::White);
-	text.setStyle(sf::Text::Bold);
-
-
+	sf::Clock deltaClock;
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
-			if (event.type == sf::Event::MouseButtonPressed) {
 			}
 		}
 
 		window.clear();
-
-		window.draw(text);
+		window.draw(shape);
 		window.display();
 	}
 
+	
 	std::cout << "Program End - Las\n";
 	return 0;
 }
