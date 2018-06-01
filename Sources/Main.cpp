17ec2dcfd08d509257dd82eb61750bb3eca5cb79
Wonho ha <35015234+Las-Wonho@users.noreply.github.com>
2018-06-01 11:52:14 +0900

    explain added

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,4 +1,13 @@
-﻿//STD
+﻿/*************************************************************************
+> File Name: Main.cpp
+> Project Name: 2048 in GSM
+> Author: Wonho Ha aka Las
+> Purpose: Main entry of 2048 Game
+> Created Time: 2018/06/01
+> Copyright (c) 2018, Wonho Ha aka Las
+*************************************************************************/
+
+//STD
 #include<iostream>
 //Other
 #include<SFML/Graphics.hpp>
@@ -16,8 +25,10 @@ int main() {
 	Wonho_pp::Resource res("../../Resources");
 	Wonho_pp::String_Helper StringHelper;
 	Wonho_pp::Screen_Move ScreenHelper;
+	
 	ScreenHelper.setTileSize(90,90,2);
 	ScreenHelper.setMapSize(800, 800, 100);
+	
 
 	sf::RenderWindow window(sf::VideoMode(800, 800), "PLAY - LAS");
 	//	
